24af3ce2dcd76e6fe34bf3df88aadc6178a310c7
serii <seriiburduja@gmail.com>
2019-11-12 17:17:38 +0200

    pdf

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
     <div class="header-top">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-7 col-sm-6 col-xs-12">
+                <div class="col-md-6 col-sm-6 col-xs-12">
                     <div class="single-drop">
                         <nav>
                             <ul>
@@ -62,7 +62,7 @@
                         </nav>
                     </div>
                 </div>
-                <div class="col-md-5 col-sm-6 col-xs-12">
+                <div class="col-md-6 col-sm-6 col-xs-12">
                     <ul class="social_list">
                         <li>
                             <a target="_blank" href="https://www.pinterest.com/president2439/">
@@ -89,6 +89,11 @@
                                 <i class="fa fa-facebook-square" aria-hidden="true"></i></a>
                         </li>
                     </ul>
+                    <ul class="pdf-list">
+                        <li>
+                            <a target="_blank" href="<?php echo carbon_get_theme_option('crb_pdf_file'); ?>"><?php echo carbon_get_theme_option('crb_pdf_title'.get_lang()); ?></a>
+                        </li>
+                    </ul>
                     <div class="single-menu">
                         <nav>
                             <ul>
@@ -112,8 +117,8 @@
             <div class="row">
                 <div class="col-md-3 col-sm-3 col-xs-12">
                     <div class="logo-area">
-                        <a href="/">
-                            <img src="http://benefis.myihor.ru/images/template/logo.png" alt=""/>
+                        <a href="<?php echo home_url(); ?>">
+                            <img src="<?php echo get_template_directory_uri().'/img/logo.svg'; ?>" alt=""/>
                         </a>
                     </div>
                 </div>
--- inc/carbon-fields/cb.php
+++ inc/carbon-fields/cb.php
@@ -1,99 +1,110 @@
 <?php
-	
-	use Carbon_Fields\Container;
-	use Carbon_Fields\Field;
-	
-	add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
-	function crb_attach_theme_options()
-	{
-		$basic_options_container = Container::make('theme_options', __('Basic Options'))
-			->add_tab(__('Contacts'), array(
-				Field::make('text', 'crb_mail', __('Email')),
-				Field::make('text', 'crb_phone_1', __('Phone 1'))
-					->set_width(30),
-				Field::make('text', 'crb_phone_2', __('Phone 2'))
-					->set_width(30),
-				Field::make('text', 'crb_phone_3', __('Phone 3'))
-					->set_width(30),
-				
-				Field::make('text', 'crb_address_ru', __('Address ru'))
-					->set_width(30),
-				Field::make('text', 'crb_address_ro', __('Address ro'))
-					->set_width(30),
-				Field::make('text', 'crb_address_en', __('Address en'))
-					->set_width(30),
-				
-				Field::make('text', 'crb_facebook', __('Facebook')),
-				Field::make('text', 'crb_twitter', __('Twitter')),
-				Field::make('text', 'crb_linkedin', __('Linked in')),
-			));
-		
-		// Add second options page under 'Basic Options'
-		Container::make('theme_options', 'Blocks')
-			->set_page_parent($basic_options_container)// reference to a top level container
-			->add_tab(__('Slider'), array(
-				Field::make('text', 'crb_slider_title_ro', __('Block slider title ro')),
-			));
-		
-		// Add second options page under 'Basic Options'
-		// Add second options page under 'Basic Options'
-		Container::make('theme_options', 'Strings')
-			->set_page_parent($basic_options_container)// reference to a top level container
-			->add_fields(array(
-				Field::make('text', 'crb_valulte_en', __('Valute en'))
-					->set_width(30),
-				Field::make('text', 'crb_valulte_ro', __('Valute ro'))
-					->set_width(30),
-				Field::make('text', 'crb_valulte_ru', __('Valute ru'))
-					->set_width(30),
 
-				Field::make('text', 'crb_language_en', __('language en'))
-					->set_width(30),
-				Field::make('text', 'crb_language_ro', __('language ro'))
-					->set_width(30),
-				Field::make('text', 'crb_language_ru', __('language ru'))
-					->set_width(30),
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
 
-				Field::make('text', 'crb_basket_ru', __('Basket ru'))
-					->set_width(30),
-				Field::make('text', 'crb_basket_ro', __('Basket ro'))
-					->set_width(30),
-				Field::make('text', 'crb_basket_en', __('Basket en'))
-					->set_width(30),
+add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
+function crb_attach_theme_options()
+{
+	$basic_options_container = Container::make('theme_options', __('Basic Options'))
+		->add_tab(__('Contacts'), array(
+			Field::make('text', 'crb_mail', __('Email')),
+			Field::make('text', 'crb_phone_1', __('Phone 1'))
+				->set_width(30),
+			Field::make('text', 'crb_phone_2', __('Phone 2'))
+				->set_width(30),
+			Field::make('text', 'crb_phone_3', __('Phone 3'))
+				->set_width(30),
 
-				Field::make('text', 'crb_summ_ru', __('Summ ru'))
-					->set_width(30),
-				Field::make('text', 'crb_summ_ro', __('Summ ro'))
-					->set_width(30),
-				Field::make('text', 'crb_summ_en', __('Summ en'))
-					->set_width(30),
+			Field::make('text', 'crb_address_ru', __('Address ru'))
+				->set_width(30),
+			Field::make('text', 'crb_address_ro', __('Address ro'))
+				->set_width(30),
+			Field::make('text', 'crb_address_en', __('Address en'))
+				->set_width(30),
 
-				Field::make('text', 'crb_to_cart_ro', __('crb_to_cart_ro'))
-					->set_width(30),
-				Field::make('text', 'crb_to_cart_ru', __('crb_to_cart_ru'))
-					->set_width(30),
-				Field::make('text', 'crb_to_cart_en', __('crb_to_cart_en'))
-					->set_width(30),
+			Field::make('text', 'crb_facebook', __('Facebook')),
+			Field::make('text', 'crb_twitter', __('Twitter')),
+			Field::make('text', 'crb_linkedin', __('Linked in')),
+		));
 
-				Field::make('text', 'crb_enter_en', __('crb_enter_en'))
-					->set_width(30),
-				Field::make('text', 'crb_enter_ro', __('crb_enter_ro'))
-					->set_width(30),
-				Field::make('text', 'crb_enter_ru', __('crb_enter_ru'))
-					->set_width(30),
+	// Add second options page under 'Basic Options'
+	Container::make('theme_options', 'Blocks')
+		->set_page_parent($basic_options_container)// reference to a top level container
+		->add_tab(__('Slider'), array(
+			Field::make('text', 'crb_slider_title_ro', __('Block slider title ro')),
+		))
+		->add_tab('Pdf', array(
+			Field::make('text', 'crb_pdf_title_en', __('crb_pdf_title_en'))
+			->set_width(30),
+			Field::make('text', 'crb_pdf_title_ro', __('crb_pdf_title_ro'))
+			->set_width(30),
+			Field::make('text', 'crb_pdf_title_ru', __('crb_pdf_title_ru'))
+			->set_width(30),
 
-				Field::make('text', 'crb_registration_en', __('crb_registration_en'))
-					->set_width(30),
-				Field::make('text', 'crb_registration_ro', __('crb_registration_ro'))
-					->set_width(30),
-				Field::make('text', 'crb_registration_ru', __('crb_registration_ru'))
-					->set_width(30),
-			));
-	}
-	
-	add_action('after_setup_theme', 'crb_load');
-	function crb_load()
-	{
-		require_once __DIR__ . '/../../vendor/autoload.php';
-		\Carbon_Fields\Carbon_Fields::boot();
-	}
+			Field::make('file', 'crb_pdf_file', __('crb_pdf_file'))
+			->set_value_type('url')
+		));
+
+	// Add second options page under 'Basic Options'
+	// Add second options page under 'Basic Options'
+	Container::make('theme_options', 'Strings')
+		->set_page_parent($basic_options_container)// reference to a top level container
+		->add_fields(array(
+			Field::make('text', 'crb_valulte_en', __('Valute en'))
+				->set_width(30),
+			Field::make('text', 'crb_valulte_ro', __('Valute ro'))
+				->set_width(30),
+			Field::make('text', 'crb_valulte_ru', __('Valute ru'))
+				->set_width(30),
+
+			Field::make('text', 'crb_language_en', __('language en'))
+				->set_width(30),
+			Field::make('text', 'crb_language_ro', __('language ro'))
+				->set_width(30),
+			Field::make('text', 'crb_language_ru', __('language ru'))
+				->set_width(30),
+
+			Field::make('text', 'crb_basket_ru', __('Basket ru'))
+				->set_width(30),
+			Field::make('text', 'crb_basket_ro', __('Basket ro'))
+				->set_width(30),
+			Field::make('text', 'crb_basket_en', __('Basket en'))
+				->set_width(30),
+
+			Field::make('text', 'crb_summ_ru', __('Summ ru'))
+				->set_width(30),
+			Field::make('text', 'crb_summ_ro', __('Summ ro'))
+				->set_width(30),
+			Field::make('text', 'crb_summ_en', __('Summ en'))
+				->set_width(30),
+
+			Field::make('text', 'crb_to_cart_ro', __('crb_to_cart_ro'))
+				->set_width(30),
+			Field::make('text', 'crb_to_cart_ru', __('crb_to_cart_ru'))
+				->set_width(30),
+			Field::make('text', 'crb_to_cart_en', __('crb_to_cart_en'))
+				->set_width(30),
+
+			Field::make('text', 'crb_enter_en', __('crb_enter_en'))
+				->set_width(30),
+			Field::make('text', 'crb_enter_ro', __('crb_enter_ro'))
+				->set_width(30),
+			Field::make('text', 'crb_enter_ru', __('crb_enter_ru'))
+				->set_width(30),
+
+			Field::make('text', 'crb_registration_en', __('crb_registration_en'))
+				->set_width(30),
+			Field::make('text', 'crb_registration_ro', __('crb_registration_ro'))
+				->set_width(30),
+			Field::make('text', 'crb_registration_ru', __('crb_registration_ru'))
+				->set_width(30),
+		));
+}
+
+add_action('after_setup_theme', 'crb_load');
+function crb_load()
+{
+	require_once __DIR__ . '/../../vendor/autoload.php';
+	\Carbon_Fields\Carbon_Fields::boot();
+}
