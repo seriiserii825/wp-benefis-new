94cbfb49ef3a81a28b4939c7e7dbe8827c64a8cf
serii <seriiburduja@gmail.com>
2019-11-13 17:51:25 +0200

    proporsal

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -62,6 +62,31 @@ get_header();
 
 				<?php else: ?><?php endif; ?>
             </div>
+
+            <h2 class="products-section__title"><?php echo get_the_category_by_ID(380); ?></h2>
+            <div class="bs-products-loops">
+		        <?php
+		        $products_proposal = new WP_Query([
+			        'post_type' => 'product',
+			        'posts_per_page' => 10,
+			        'product_cat' => 'new-proposals'
+		        ]);
+		        ?>
+
+		        <?php if ($products_proposal->have_posts()): ?>
+
+			        <?php while ($products_proposal->have_posts()): ?><?php $products_proposal->the_post(); ?>
+
+				        <?php do_action('woocommerce_product_loop_start'); ?>
+
+				        <?php wc_get_template_part('content', 'product'); ?>
+
+				        <?php do_action('woocommerce_product_loop_end'); ?>
+
+			        <?php endwhile; ?><?php wp_reset_postdata(); ?>
+
+		        <?php else: ?><?php endif; ?>
+            </div>
         </div>
     </div>
 <?php
